d1c1fc8aec5d2785e32730c3d74b138ea6f52a2f
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-03-28 17:52:16 -0700

    Concluido

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,105 @@ namespace Actividad_Integradora_2_Problema_4
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnContinuar = new System.Windows.Forms.Button();
+            this.txtPedido = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblEnvio = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monto del Pedido:";
+            // 
+            // btnContinuar
+            // 
+            this.btnContinuar.Location = new System.Drawing.Point(306, 235);
+            this.btnContinuar.Name = "btnContinuar";
+            this.btnContinuar.Size = new System.Drawing.Size(188, 58);
+            this.btnContinuar.TabIndex = 1;
+            this.btnContinuar.Text = "Continuar";
+            this.btnContinuar.UseVisualStyleBackColor = true;
+            this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
+            // 
+            // txtPedido
+            // 
+            this.txtPedido.Location = new System.Drawing.Point(456, 124);
+            this.txtPedido.Name = "txtPedido";
+            this.txtPedido.Size = new System.Drawing.Size(250, 47);
+            this.txtPedido.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 399);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 41);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Envio:  ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(119, 463);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 41);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total a Pagar: ";
+            // 
+            // lblEnvio
+            // 
+            this.lblEnvio.AutoSize = true;
+            this.lblEnvio.Location = new System.Drawing.Point(214, 399);
+            this.lblEnvio.Name = "lblEnvio";
+            this.lblEnvio.Size = new System.Drawing.Size(50, 41);
+            this.lblEnvio.TabIndex = 5;
+            this.lblEnvio.Text = "$$";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(326, 463);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(50, 41);
+            this.lblTotal.TabIndex = 6;
+            this.lblTotal.Text = "$$";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(836, 647);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblEnvio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPedido);
+            this.Controls.Add(this.btnContinuar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Actividad Integradora 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnContinuar;
+        private System.Windows.Forms.TextBox txtPedido;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblEnvio;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,33 @@ namespace Actividad_Integradora_2_Problema_4
         public Form1()
         {
             InitializeComponent();
+            lblEnvio.Text = "0";
+            lblTotal.Text = "0";
         }
 
+        private void btnContinuar_Click(object sender, EventArgs e)
+        {
+            double cantidad = Convert.ToDouble(txtPedido.Text);
+            double envio = 0.0;
+            double total = 0.0;
+
+            if (cantidad < 3000)
+            {
+                envio = cantidad * 0.20;
+                total = cantidad + envio;
+                lblEnvio.Text = string.Format("$ {0}", envio);
+                lblTotal.Text = string.Format("$ {0}", total);
+            }
+            else if( cantidad >= 3000 && cantidad <20000 )
+            {
+                lblEnvio.Text = "Gratis";
+                lblTotal.Text = string.Format("$ {0}", cantidad);
+            }
+            else if(cantidad >= 20000)
+            {
+                lblEnvio.Text = "¡¡¡¡¡Enviar por transferencia!!!!!";
+                lblTotal.Text = string.Format("$ {0}", cantidad);
+            }
+        }
     }
 }
